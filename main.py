74f08d49c6d3e16d56220c9d50450605cc3a183a
miyan1221 <103588276+miyan1221@users.noreply.github.com>
2023-06-15 21:32:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from os import getenv
 
 from pandora_cloud.server import ChatBot
-import subprocess
+
 
 
 _port = getenv('PORT')
@@ -12,5 +12,5 @@ _debug = getenv('PANDORA_DEBUG', 'false').lower() == 'true'
 _sentry = getenv('PANDORA_SENTRY', 'false').lower() == 'true'
 _listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
 _server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
-subprocess.run(["node", "script.js"])
+
 app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True).run(_server, listen=_listen)

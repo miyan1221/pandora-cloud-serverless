d530cdfe97d71b0108bec01a1394f9e3bb0dfd5e
miyan1221 <103588276+miyan1221@users.noreply.github.com>
2023-06-15 19:04:04 +0800

    Update main.py
    尝试隐藏指定class

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 from os import getenv
 
 from pandora_cloud.server import ChatBot
+import subprocess
+
+
 
 _port = getenv('PORT')
 _proxy = getenv('PANDORA_PROXY')
@@ -12,3 +15,4 @@ _listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
 _server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
 
 app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True).run(_server, listen=_listen)
+subprocess.run(["node", "script.js"])
